155fe809589e58cb3f2c5c6cb2a80c34f3ab3479
venku31 <venku31@gmail.com>
2025-04-08 11:28:01 +0530

    create delivery invoice changes

--- sepl/public/js/quotation.js
+++ sepl/public/js/quotation.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Quotation', {
     refresh(frm) {
         if (frm.doc.docstatus === 1) {
-            frm.add_custom_button(__('Create Delivery Note'), () => {
+            frm.add_custom_button(__('Delivery Invoice & Packing Slip'), () => {
             frappe.call({
     method: "sepl.api.delivery_note.create_delivery_note_from_quotation",
     args: {
